12d9914d3d1f3a333f1d096ace9dc56bb631be3b
naufalbasara <naufalbasara@outlook.com>
2024-01-15 07:34:03 +0700

    update: add async to fetch function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,46 +6,49 @@ import Card from '@/components/Card';
 import { Poppins } from '@next/font/google';
 
 const poppins = Poppins({
-  subsets:['latin'],
-  weight:['400', '700'],
-})
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 export default function HomePage() {
   const [isLoading, setLoading] = React.useState(false);
-  const [gitData, setGitData] = React.useState({ 
-    username: null, total_repos: null, avatar_url: null,
+  const [gitData, setGitData] = React.useState({
+    username: null,
+    total_repos: null,
+    avatar_url: null,
   });
-  // const [xdata, setXData] = React.useState( {username: null, profile_image_url:null} );
-  
-  const dataFetchedRef = React.useRef(false);
 
   React.useEffect(() => {
-    if (dataFetchedRef.current) return;
-    dataFetchedRef.current = true;
-    try {
-      setLoading(true)
-      fetch('api/handler').then((response) => {
-        response.json().then((response) => {
-          const github_data = {
-            username: response.name,
-            total_repos: response.total_repos,
-            avatar_url: response.avatar_url
-          };
-          setGitData(github_data);
-          setLoading(false)
+    (async () => {
+      try {
+        setLoading(true);
+        await fetch('api/handler').then((response) => {
+          response.json().then((response) => {
+            const github_data = {
+              username: response.name,
+              total_repos: response.total_repos,
+              avatar_url: response.avatar_url,
+            };
+            setGitData(github_data);
+            setLoading(false);
+          });
         });
-      });
-    } catch {
-      console.log('failed to fetch');
-    }
+      } catch {
+        console.log('failed to fetch');
+      }
+    })();
   }, []);
 
   return (
     <Layout>
-      <Seo/>
+      <Seo />
       <section className='flex items-center'>
-        <img alt='' className='block object-cover mx-auto w-28 shrink-0 h-28 rounded-full bg-white' src='/images/profile-picture.JPG'/>
-        <div className='h-full p-2 text-left sm:h-20 bg-[#2A412F] rounded-2xl container'>
+        <img
+          alt=''
+          className='mx-auto block h-28 w-28 shrink-0 rounded-full bg-white object-cover'
+          src='/images/profile-picture.JPG'
+        />
+        <div className='container h-full rounded-2xl bg-[#2A412F] p-2 text-left sm:h-20'>
           <h1 className='mb-2 text-lg sm:text-3xl'>Naufal Rafiawan Basara</h1>
           <p className='text-xs text-[#A0A0A0] sm:text-sm'>
             Tech savvy, AI/ML enthusiast
@@ -77,24 +80,45 @@ export default function HomePage() {
           <i className='devicon-microsoftsqlserver-plain-wordmark mr-4'></i>
           <i className='devicon-nextjs-original'></i>
         </div>
-        <div className={isLoading ? 'animate-pulse my-8' : ' my-8'}>
-            <Card href='https://github.com/naufalbasara' className={'flex h-24 w-60 items-center justify-between p-4 text-left hover:scale-105 transition hover:bg-opacity-70 hover:backdrop-blur-md hover:drop-shadow-md'}>
-                <img src={isLoading ? '/images/profile-picture.png' : gitData.avatar_url!} className='h-12 w-12' alt="" />
-              <div className={isLoading ? 'animate-pulse flex-1 space-y-3 py-1' : ""}>
-                <p className={isLoading ? 'h-2 bg-slate-400 rounded' : ''}>{!isLoading && gitData.username}</p>
-                <p className={isLoading ? 'h-2 bg-slate-400 rounded ' : 'text-xs text-[#A0A0A0]'}>
-                  {!isLoading && gitData.total_repos + ' total repositories'}
-                </p>
-              </div>
-              <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  height='32'
-                  viewBox='0 -960 960 960'
-                  width='32'
-                >
-                  <path d='M180-120q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h279v60H180v600h600v-279h60v279q0 24-18 42t-42 18H180Zm202-219-42-43 398-398H519v-60h321v321h-60v-218L382-339Z' />
-                </svg>
-            </Card>
+        <div className={isLoading ? 'my-8 animate-pulse' : ' my-8'}>
+          <Card
+            href='https://github.com/naufalbasara'
+            className={
+              'flex h-24 w-60 items-center justify-between p-4 text-left transition hover:scale-105 hover:bg-opacity-70 hover:drop-shadow-md hover:backdrop-blur-md'
+            }
+          >
+            <img
+              src={
+                isLoading ? '/images/profile-picture.png' : gitData.avatar_url!
+              }
+              className='h-12 w-12'
+              alt=''
+            />
+            <div
+              className={isLoading ? 'flex-1 animate-pulse space-y-3 py-1' : ''}
+            >
+              <p className={isLoading ? 'h-2 rounded bg-slate-400' : ''}>
+                {!isLoading && gitData.username}
+              </p>
+              <p
+                className={
+                  isLoading
+                    ? 'h-2 rounded bg-slate-400 '
+                    : 'text-xs text-[#A0A0A0]'
+                }
+              >
+                {!isLoading && gitData.total_repos + ' total repositories'}
+              </p>
+            </div>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              height='32'
+              viewBox='0 -960 960 960'
+              width='32'
+            >
+              <path d='M180-120q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h279v60H180v600h600v-279h60v279q0 24-18 42t-42 18H180Zm202-219-42-43 398-398H519v-60h321v321h-60v-218L382-339Z' />
+            </svg>
+          </Card>
         </div>
       </section>
     </Layout>
