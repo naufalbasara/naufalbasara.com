04104b21f08eacbaf577e9061f514dfc67971428
naufalbasara <naufalbasara@outlook.com>
2024-01-20 01:59:45 +0700

    update: readme markdown

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -4,9 +4,7 @@ type OpenGraphType = {
   templateTitle?: string;
   logo?: string;
 };
-// !STARTERCONF This OG is generated from https://github.com/theodorusclarence/og
-// Please clone them and self-host if your site is going to be visited by many people.
-// Then change the url and the default logo.
+
 export function openGraph({
   siteName,
   templateTitle,
@@ -21,7 +19,7 @@ export function openGraph({
     : undefined;
   const ogDesc = encodeURIComponent(description.trim());
 
-  return `https://og.<your-domain>/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
+  return `https://og.naufalbasara.engineer/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
     ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ''
   }`;
 }
