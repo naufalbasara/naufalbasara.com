07610c734f9c08fda2e81edc6f572deb0a1299be
naufalbasara <naufalbasara@outlook.com>
2024-01-21 04:36:31 +0700

    fix: minor changes to the env key

--- src/components/Discussion.tsx
+++ src/components/Discussion.tsx
@@ -6,8 +6,8 @@ export default function Discussion() {
   return (
     <Giscus
           id="comments"
-          repo={(process.env.GISCUS_REPO as Repo)}
-          repoId={process.env.GISCUS_REPO || ''}
+          repo={(process.env.NEXT_PUBLIC_GISCUS_REPO as Repo)}
+          repoId={process.env.NEXT_PUBLIC_GISCUS_REPO_ID || ''}
           category="General"
           categoryId="DIC_kwDOJNLybs4CcjBQ"
           mapping="pathname"
--- src/pages/guestbook.tsx
+++ src/pages/guestbook.tsx
@@ -3,19 +3,26 @@ import * as React from 'react';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
 import Discussion from '@/components/Discussion';
+import { Poppins } from '@next/font/google';
+
+const poppins = Poppins({
+  subsets:['latin'],
+  weight:['400', '700'],
+})
 
 export default function GuestBook() {
-
   return (
     <Layout>
       <Seo />
-      <section className='block items-start text-left'>
+      <section className={poppins.className + ' block items-start text-left'}>
         <h1 className='mb-6 text-left text-lg sm:text-3xl'>
         Sign my guestbook
         </h1>
-        <p className='text-[#A0A0A0]'>Leave an online footprint here! - suggestions or messages</p>
+        <p className='text-xs text-[#A0A0A0]'>Leave an online footprint here - any suggestions or appreciations are welcomed</p>
       </section>
-      <Discussion/>
+      <section className='pt-6 pb-20'>
+        <Discussion/>
+      </section>
     </Layout>
   );
 }
